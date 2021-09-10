25f433f8bd3aecdecbe58e3b0727e2a00004fff1
Ángel Barrera Sánchez <angel@sighup.io>
2021-09-10 13:20:06 +0200

    Test with newer e2e environment

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bats
 
 load "./lib/helper"
+
 @test "[SETUP] Deploy Permission manager Requirements" {
     info
     deploy(){
@@ -21,18 +22,34 @@ load "./lib/helper"
 
 }
 
+@test "DEBUG SECRETS" {
+    deploy(){
+        cat tests/manifests/permission-manager-secret.yml | envsubst | kubectl apply -f -
+    }
+    run deploy
+    echo "$output" >&3
+    [ "$status" -eq 0 ]
+}
+
 @test "[SETUP] Build and Deploy Permission manager. It may take some time." {
     info
 
     deploy(){
       # we build the permission image from the current data and run the tests against it
-      make build
       make deploy
-      kubectl wait --for=condition=Available deploy/permission-manager -n permission-manager --timeout=300s
-
     }
 
     run deploy
+    echo "$output" >&3
     [ "$status" -eq 0 ]
 
+}
+
+@test "[SETUP] DEBUG" {
+    info
+    kubectl get all >&3
+    kubectl get deploy -o yaml -n permission-manager >&3
+    kubectl get secrets -n permission-manager >&3
+    kubectl get pods -o wide -n permission-manager >&3
+    kubectl describe pod -n permission-manager >&3
 }
